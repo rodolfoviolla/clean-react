9c9fda7e568081a5aeb7d9ed2b953dab3aa23b24
Rodolfo Violla <rodolfo.violla@gmail.com>
2022-07-17 11:34:56 -0300

    feat: ensures RemoteAuthentication throws UnexpectedError on default cases

--- src/data/useCases/authentication/remoteAuthentication.ts
+++ src/data/useCases/authentication/remoteAuthentication.ts
@@ -1,6 +1,7 @@
 import { HttpPostClient } from '@/data/protocols/http/httpPostClient'
 import { HttpStatusCode } from '@/data/protocols/http/httpResponse'
 import { InvalidCredentialsError } from '@/domain/errors/invalidCredentialsError'
+import { UnexpectedError } from '@/domain/errors/unexpectedError'
 import { AuthenticationParams } from '@/domain/useCases/authentication'
 
 export class RemoteAuthentication {
@@ -10,7 +11,9 @@ export class RemoteAuthentication {
     const httpResponse = await this.httpPostClient.post({ url: this.url, body: params })
 
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
+      default: throw new UnexpectedError()
     }
   }
 }
