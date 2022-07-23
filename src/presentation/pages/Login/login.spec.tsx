1f3a4eea254a33c82dc8c141993bd6b23b2e376d
Rodolfo Violla <rodolfo.violla@gmail.com>
2022-07-23 17:28:42 -0300

    test: ensures Login shows spinner on submit

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -91,4 +91,17 @@ describe('Login Component', () => {
     const submitButton = getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBeFalsy()
   })
+
+  test('Should show spinner on submit', () => {
+    const { getByTestId } = makeSut()
+
+    const emailInput = getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+    const passwordInput = getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+    const submitButton = getByTestId('submit')
+    fireEvent.click(submitButton)
+    const spinner = getByTestId('spinner')
+    expect(spinner).toBeTruthy()
+  })
 })
