c508a014406d4034110c42e629fe6c8b078aa750
Rodolfo Violla <rodolfo.violla@gmail.com>
2022-07-26 22:32:08 -0300

    test: ensures ValidationComposite return the first error found

--- src/validation/validators/composite/validationComposite.spec.ts
+++ src/validation/validators/composite/validationComposite.spec.ts
@@ -11,4 +11,19 @@ describe('ValidationComposite', () => {
     const error = sut.validate('any_field', 'any_value')
     expect(error).toBe('any_error_message')
   })
+
+  test('Should stop validation if one of them fails', () => {
+    const fieldValidationSpyWithSuccess = new FieldValidationSpy('any_field')
+    const fieldValidationSpyWithFirstError = new FieldValidationSpy('any_field')
+    const fieldValidationSpyWithSecondError = new FieldValidationSpy('any_field')
+    fieldValidationSpyWithFirstError.error = new Error('first_error_message')
+    fieldValidationSpyWithSecondError.error = new Error('second_error_message')
+    const sut = new ValidationComposite([
+      fieldValidationSpyWithSuccess,
+      fieldValidationSpyWithFirstError,
+      fieldValidationSpyWithSecondError
+    ])
+    const error = sut.validate('any_field', 'any_value')
+    expect(error).toBe('first_error_message')
+  })
 })
