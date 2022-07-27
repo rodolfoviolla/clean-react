465ba813fdbaad1f83032eea6a1a8042dec56c28
Rodolfo Violla <rodolfo.violla@gmail.com>
2022-07-26 22:40:30 -0300

    refactor: moves SUT creation to a factory method

--- src/validation/validators/composite/validationComposite.spec.ts
+++ src/validation/validators/composite/validationComposite.spec.ts
@@ -2,27 +2,30 @@ import { FieldValidationSpy } from '@/validation/test/mockFieldValidation'
 
 import { ValidationComposite } from '.'
 
+type SutTypes = {
+  sut: ValidationComposite
+  fieldValidationsSpy: FieldValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+  const fieldValidationsSpy = [new FieldValidationSpy('any_field'), new FieldValidationSpy('any_field')]
+  const sut = new ValidationComposite(fieldValidationsSpy)
+
+  return { sut, fieldValidationsSpy }
+}
+
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const fieldValidationSpyWithSuccess = new FieldValidationSpy('any_field')
-    const fieldValidationSpyWithError = new FieldValidationSpy('any_field')
-    fieldValidationSpyWithError.error = new Error('any_error_message')
-    const sut = new ValidationComposite([fieldValidationSpyWithSuccess, fieldValidationSpyWithError])
+    const { sut, fieldValidationsSpy } = makeSut()
+    fieldValidationsSpy[1].error = new Error('any_error_message')
     const error = sut.validate('any_field', 'any_value')
     expect(error).toBe('any_error_message')
   })
 
   test('Should stop validation if one of them fails', () => {
-    const fieldValidationSpyWithSuccess = new FieldValidationSpy('any_field')
-    const fieldValidationSpyWithFirstError = new FieldValidationSpy('any_field')
-    const fieldValidationSpyWithSecondError = new FieldValidationSpy('any_field')
-    fieldValidationSpyWithFirstError.error = new Error('first_error_message')
-    fieldValidationSpyWithSecondError.error = new Error('second_error_message')
-    const sut = new ValidationComposite([
-      fieldValidationSpyWithSuccess,
-      fieldValidationSpyWithFirstError,
-      fieldValidationSpyWithSecondError
-    ])
+    const { sut, fieldValidationsSpy } = makeSut()
+    fieldValidationsSpy[0].error = new Error('first_error_message')
+    fieldValidationsSpy[1].error = new Error('second_error_message')
     const error = sut.validate('any_field', 'any_value')
     expect(error).toBe('first_error_message')
   })
