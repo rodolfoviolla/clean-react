8d7994cfb123c552ca132e0515e83ecc3363b293
Rodolfo Violla <rodolfo.violla@gmail.com>
2022-07-27 12:57:20 -0300

    refactor: fixes HttpPostClient generics

--- src/infra/http/axiosHttpClient/index.ts
+++ src/infra/http/axiosHttpClient/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 import { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http'
 
-export class AxiosHttpClient<T, R> implements HttpPostClient<T, R> {
+export class AxiosHttpClient<T = any, R = any> implements HttpPostClient<T, R> {
   async post (params: HttpPostParams<T>): Promise<HttpResponse<R>> {
     const { status: statusCode, data: body } = await axios.post(params.url, params.body)
 
