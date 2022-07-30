04033c8fe583f8c80142e35091c95f52c6abda10
Rodolfo Violla <rodolfo.violla@gmail.com>
2022-07-30 15:14:05 -0300

    feat: ensures RemoteAddAccount calls HttpPostClient with correct url

---
+++ src/data/useCases/addAccount/remoteAddAccount.ts
@@ -0,0 +1,16 @@
+import { HttpPostClient } from '@/data/protocols/http'
+import { AccountModel } from '@/domain/models/accountModel'
+import { AddAccount, AddAccountParams } from '@/domain/useCases/addAccount'
+
+export class RemoteAddAccount implements AddAccount {
+  constructor (
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient<AddAccountParams, AccountModel>
+  ) {}
+
+  async add (params: AddAccountParams): Promise<AccountModel> {
+    await this.httpPostClient.post({ url: this.url })
+
+    return null
+  }
+}
