b2a32700e45b79c98a74cca27b141290bcb8e6f5
Rodolfo Violla <rodolfo.violla@gmail.com>
2022-07-30 17:53:15 -0300

    test: ensures RemoteAddAccount throws UnexpectedError if HttpPostClient returns 500

--- src/data/useCases/addAccount/remoteAddAccount.spec.ts
+++ src/data/useCases/addAccount/remoteAddAccount.spec.ts
@@ -66,4 +66,13 @@ describe('RemoteAddAccount', () => {
     const promise = sut.add(mockAddAccount())
     await expect(promise).rejects.toThrow(new EmailInUseError())
   })
+
+  test('Should throw EmailInUseError if HttpPostClient returns 500', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.add(mockAddAccount())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
