537244072510a87ed62d84cc39d2a742f501605a
Rodolfo Violla <rodolfo.violla@gmail.com>
2022-07-30 14:40:07 -0300

    feat: adds AddAccount useCase

---
+++ src/domain/useCases/addAccount.ts
@@ -0,0 +1,12 @@
+import { AccountModel } from '@/domain/models/accountModel'
+
+export type AddAccountParams = {
+  name: string
+  email: string
+  password: string
+  passwordConfirmation: string
+}
+
+export interface AddAccount {
+  add: (params: AddAccountParams) => Promise<AccountModel>
+}
