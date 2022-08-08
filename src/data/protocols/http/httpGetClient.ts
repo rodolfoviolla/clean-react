129529b6318aa020f4be3f3eb27769332ed821ca
Rodolfo Violla <rodolfo.violla@gmail.com>
2022-08-08 20:14:14 -0300

    feat: ensures RemoteLoadSurveyList throws UnexpectedError on 403

--- src/data/protocols/http/httpGetClient.ts
+++ src/data/protocols/http/httpGetClient.ts
@@ -1,7 +1,9 @@
+import { HttpResponse } from './httpResponse'
+
 export type HttpGetParams = {
   url: string
 }
 
-export interface HttpGetClient {
-  get: (params: HttpGetParams) => Promise<void>
+export interface HttpGetClient<ResponseType> {
+  get: (params: HttpGetParams) => Promise<HttpResponse<ResponseType>>
 }
