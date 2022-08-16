72db2039fd0c28e6c97f80362ffb78a68a37105c
Rodolfo Violla <rodolfo.violla@gmail.com>
2022-08-16 19:43:31 -0300

    refactor: changes how AuthorizeHttpGetClientDecorator works

--- src/main/decorators/authorizeHttpGetClient/authorizeHttpGetClientDecorator.ts
+++ src/main/decorators/authorizeHttpGetClient/authorizeHttpGetClientDecorator.ts
@@ -4,7 +4,7 @@ import { HttpGetClient, HttpGetParams } from '@/data/protocols/http'
 export class AuthorizeHttpGetClientDecorator<ResponseType> implements HttpGetClient<ResponseType> {
   constructor (
     private readonly getStorage: GetStorage,
-    private readonly httpGetClientSpy: HttpGetClient<ResponseType>
+    private readonly httpGetClient: HttpGetClient<ResponseType>
   ) {}
 
   async get (params: HttpGetParams) {
@@ -12,10 +12,10 @@ export class AuthorizeHttpGetClientDecorator<ResponseType> implements HttpGetCli
 
     if (account?.accessToken) {
       Object.assign(params, {
-        headers: Object.assign(params.headers || {}, { 'x-access-token': account.accessToken })
+        headers: { ...(params.headers || {}), 'x-access-token': account.accessToken }
       })
     }
 
-    return await this.httpGetClientSpy.get(params)
+    return await this.httpGetClient.get(params)
   }
 }
