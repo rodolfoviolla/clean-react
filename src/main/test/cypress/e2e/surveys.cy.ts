76be6db0ad8dce7658758c259d3e4b06235b6517
Rodolfo Violla <rodolfo.violla@gmail.com>
2022-08-19 21:59:43 -0300

    test: ensures Surveys logout on AccessDeniedError

--- src/main/test/cypress/e2e/surveys.cy.ts
+++ src/main/test/cypress/e2e/surveys.cy.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker'
 
-import { setLocalStorageItem } from '../support/helpers'
+import { setLocalStorageItem, testUrl } from '../support/helpers'
 import { getAnyOtherErrorStatusCodeThan } from '../support/mockHttp'
 
 describe('Surveys', () => {
@@ -12,5 +12,11 @@ describe('Surveys', () => {
     cy.intercept('GET', /surveys/, { statusCode: getAnyOtherErrorStatusCodeThan([403]) })
     cy.visit('/')
     cy.getByTestId('error-message').should('contain.text', 'Ocorreu um erro inesperado. Tente novamente mais tarde')
+  })
+
+  it('Should logout on AccessDeniedError', () => {
+    cy.intercept('GET', /surveys/, { statusCode: 403 })
+    cy.visit('/')
+    testUrl('/login')
   })
 })
