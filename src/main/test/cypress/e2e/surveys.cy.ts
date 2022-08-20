0adfb1e58a0080cce638cf09057460da345f4d91
Rodolfo Violla <rodolfo.violla@gmail.com>
2022-08-20 11:31:30 -0300

    test: ensures Surveys reload on button click

--- src/main/test/cypress/e2e/surveys.cy.ts
+++ src/main/test/cypress/e2e/surveys.cy.ts
@@ -1,6 +1,8 @@
+import { RouteHandler } from 'cypress/types/net-stubbing'
+
 import { getLocalStorageItem, setLocalStorageItem, testUrl, getAnyOtherErrorStatusCodeThan } from '../utils'
 
-const path = /surveys/
+const mockHttpResponse = (response: RouteHandler) => cy.intercept('GET', /surveys/, response)
 
 describe('Surveys', () => {
   beforeEach(() => {
@@ -8,28 +10,58 @@ describe('Surveys', () => {
   })
 
   it('Should present error on UnexpectedError', () => {
-    cy.intercept('GET', path, { statusCode: getAnyOtherErrorStatusCodeThan([403]) })
+    mockHttpResponse({ statusCode: getAnyOtherErrorStatusCodeThan([403]) })
     cy.visit('/')
     cy.getByTestId('error-message').should('contain.text', 'Ocorreu um erro inesperado. Tente novamente mais tarde')
   })
 
+  it('Should reload on button click', () => {
+    mockHttpResponse({ statusCode: getAnyOtherErrorStatusCodeThan([403]) })
+    cy.visit('/')
+    cy.getByTestId('error-message').should('contain.text', 'Ocorreu um erro inesperado. Tente novamente mais tarde')
+    mockHttpResponse({ statusCode: 200, fixture: 'surveys' })
+    cy.getByTestId('retry').click()
+    cy.get('li:not(:empty)').should('have.length', 2)
+  })
+
   it('Should logout on AccessDeniedError', () => {
-    cy.intercept('GET', path, { statusCode: 403 })
+    mockHttpResponse({ statusCode: 403 })
     cy.visit('/')
     testUrl('/login')
   })
 
   it('Should present correct username', () => {
-    cy.intercept('GET', path, { statusCode: getAnyOtherErrorStatusCodeThan([403]) })
+    mockHttpResponse({ statusCode: getAnyOtherErrorStatusCodeThan([403]) })
     cy.visit('/')
     const { name } = getLocalStorageItem('account')
     cy.getByTestId('username').should('contain.text', name)
   })
 
   it('Should logout on logout link click', () => {
-    cy.intercept('GET', path, { statusCode: getAnyOtherErrorStatusCodeThan([403]) })
+    mockHttpResponse({ statusCode: getAnyOtherErrorStatusCodeThan([403]) })
     cy.visit('/')
     cy.getByTestId('logout').click()
     testUrl('/login')
+  })
+
+  it('Should present survey items', () => {
+    mockHttpResponse({ statusCode: 200, fixture: 'surveys' })
+    cy.visit('/')
+    cy.get('li:empty').should('have.length', 4)
+    cy.get('li:not(:empty)').should('have.length', 2)
+    cy.get('li:nth-child(1)').then(li => {
+      assert.equal(li.find('[data-testid="day"]').text(), '03')
+      assert.equal(li.find('[data-testid="month"]').text(), 'fev')
+      assert.equal(li.find('[data-testid="year"]').text(), '2022')
+      assert.equal(li.find('[data-testid="question"]').text(), 'any_question_1')
+      cy.fixture('icon').then(icon => assert.equal(li.find('[data-testid="icon"]').attr('src'), icon.thumbUp))
+    })
+    cy.get('li:nth-child(2)').then(li => {
+      assert.equal(li.find('[data-testid="day"]').text(), '05')
+      assert.equal(li.find('[data-testid="month"]').text(), 'mar')
+      assert.equal(li.find('[data-testid="year"]').text(), '2022')
+      assert.equal(li.find('[data-testid="question"]').text(), 'any_question_2')
+      cy.fixture('icon').then(icon => assert.equal(li.find('[data-testid="icon"]').attr('src'), icon.thumbDown))
+    })
   })
 })
