ff76857656a0ad5f09a173df5a057992df3d1352
Rodolfo Violla <rodolfo.violla@gmail.com>
2022-08-31 20:34:32 -0300

    feat: ensures RemoteLoadSurveyResult throws AccessDeniedError on 403

--- src/data/useCases/loadSurveyResult/remoteLoadSurveyResult.ts
+++ src/data/useCases/loadSurveyResult/remoteLoadSurveyResult.ts
@@ -1,4 +1,5 @@
-import { HttpGetClient } from '@/data/protocols/http'
+import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
+import { AccessDeniedError } from '@/domain/errors'
 import { LoadSurveyResult } from '@/domain/useCases'
 
 export class RemoteLoadSurveyResult {
@@ -8,6 +9,10 @@ export class RemoteLoadSurveyResult {
   ) {}
 
   async load () {
-    await this.httpGetCliente.get({ url: this.url })
+    const httpResponse = await this.httpGetCliente.get({ url: this.url })
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.forbidden: throw new AccessDeniedError()
+      default: break
+    }
   }
 }
